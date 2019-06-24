3082daa2b7e700e5de7ceae742000318d3c15fe8
Alexander <sphc@yandex.ru>
2019-06-25 00:00:03 +0300

    refactor

--- app/src/main/java/com/mincor/mvvmclean/view/base/mvp/IBaseView.kt
+++
@@ -1,9 +0,0 @@
-package com.mincor.room.presentation.base.mvp
-
-/**
- * Created by a.minkin on 25.10.2017.
- */
-interface IBaseView<out T : IBasePresenter<*>> {
-    val presenter: T
-    fun showError(error: String?)
-}
