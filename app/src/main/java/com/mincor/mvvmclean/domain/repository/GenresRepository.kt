1ba8270dd96002db8662b2944851b191a36c6c9d
Alexander <sphc@yandex.ru>
2019-06-28 00:13:05 +0300

    separate use cases

--- app/src/main/java/com/mincor/mvvmclean/domain/repository/GenresRepository.kt
+++ app/src/main/java/com/mincor/mvvmclean/domain/repository/GenresRepository.kt
@@ -5,18 +5,19 @@ import com.mincor.mvvmclean.common.dto.successResult
 import com.mincor.mvvmclean.datasource.local.IGenresLocalDataSource
 import com.mincor.mvvmclean.datasource.remote.IGenresRemoteDataSource
 import com.mincor.mvvmclean.domain.model.local.GenreEntity
+import com.mincor.mvvmclean.domain.model.remote.GenreModel
 
 class GenresRepository(
     private val remoteDataSource: IGenresRemoteDataSource,
     private val localDataSource: IGenresLocalDataSource
 
 ) {
-    suspend fun getLocalGenreList(): SResult.Success<List<GenreEntity>>
-            = successResult(localDataSource.getGenresList())
+    suspend fun getLocalGenreList(): SResult.Success<List<GenreEntity>> =
+        successResult(localDataSource.getGenresList())
 
     suspend fun saveGenres(genresList: List<GenreEntity>) =
         localDataSource.insertGenres(genresList)
 
-    suspend fun getRemoteGenresList() = remoteDataSource
-        .getRemoteGenresList()
+    suspend fun getRemoteGenresList(): SResult<List<GenreModel>> =
+        remoteDataSource.getRemoteGenresList()
 }
