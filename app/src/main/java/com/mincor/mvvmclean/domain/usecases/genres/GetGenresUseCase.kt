79dba562d8d95591ae2f5a13ecc96b59f6dae86a
Alexander <sphc@yandex.ru>
2019-06-28 03:27:06 +0300

    complex refactor

--- app/src/main/java/com/mincor/mvvmclean/domain/usecases/genres/GetGenresUseCase.kt
+++ app/src/main/java/com/mincor/mvvmclean/domain/usecases/genres/GetGenresUseCase.kt
@@ -9,14 +9,9 @@ class GetGenresUseCase(
     private val getRemoteGenresUseCase: GetRemoteGenresUseCase
 ) {
     suspend fun execute(): SResult<List<GenreUI>> {
-        val localResultList =
-            getLocalGenresUseCase.execute()
-        val genresResult =
-            if (localResultList.data.isNotEmpty()) {
-                localResultList
-            } else {
-                getRemoteGenresUseCase.execute()
-            }
-        return genresResult.mapListTo()
+        return getLocalGenresUseCase.execute().let { localResultList ->
+            if(localResultList.data.isNotEmpty()) localResultList
+            else getRemoteGenresUseCase.execute()
+        }.mapListTo()
     }
 }
