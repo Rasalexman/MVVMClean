b5ac7839f8c689079090401ba7af5d830ad095b9
Alexander <sphc@yandex.ru>
2019-07-12 01:38:41 +0300

    small refactoring

--- app/src/main/java/com/mincor/mvvmclean/datasource/local/GenresLocalDataSource.kt
+++ app/src/main/java/com/mincor/mvvmclean/datasource/local/GenresLocalDataSource.kt
@@ -7,7 +7,18 @@ import com.mincor.mvvmclean.providers.database.dao.IGenresDao
 
 class GenresLocalDataSource(
     private val genresDao: IGenresDao
-) : IGenresLocalDataSource {
+) : IGenresLocalDataSource, IGenresMemoryDataSource {
+
+    private val memoryGenresList = mutableListOf<GenreEntity>()
+
     override suspend fun getGenresList(): SResult<List<GenreEntity>> = successResult(genresDao.getAll())
     override suspend fun insertGenres(data: List<GenreEntity>) = genresDao.insertAll(data)
+
+    override suspend fun putGenresIntoMemoryCache(genresList: List<GenreEntity>) {
+        memoryGenresList.addAll(genresList)
+    }
+
+    override suspend fun getGenresFromMemoryCache(): SResult<List<GenreEntity>> {
+        return successResult(memoryGenresList)
+    }
 }
